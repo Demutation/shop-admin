b5f8d96ba96e36d31c0ded0f4ca24ecff52b4696
Demutation <502015422@qq.com>
2019-08-21 09:40:53 +0800

    管理页编辑有个bug待解决

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="users">
-  <!-- 面包屑 -->
+    <!-- 面包屑 -->
     <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
-  <!-- 搜索框 -->
-   <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
+    <!-- 搜索框 -->
+    <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
     <el-button @click="search" slot="append" icon="el-icon-search"></el-button>
-  </el-input>
+    </el-input>
     <el-button plain type="success" class="addBtn" @click="add">添加用户</el-button>
-  <!-- 表格 -->
+    <!-- 表格 -->
     <el-table :data="tableData" style="width: 100%" border>
       <el-table-column prop="username" label="姓名" width></el-table-column>
       <!-- el-table-column不设置宽度 是自适应 -->
@@ -33,16 +33,16 @@
       <el-table-column prop label="操作">
         <!-- 是三个不同type的button  需要绑定id进行后续操作 使用作用域插槽-->
         <template v-slot:default="obj">
-          <el-button icon="el-icon-edit" size="small" type="primary" plain></el-button>
+          <el-button @click="showEditDialog(obj.row)" icon="el-icon-edit" size="small" type="primary" plain></el-button>
           <el-button @click="deleteUsers(obj.row.id)" icon="el-icon-delete" size="small" type="danger" plain></el-button>
           <el-button icon="el-icon-check" type="success" plain>分配角色</el-button>
         </template>
       </el-table-column>
     </el-table>
-  <!-- 分页 -->
-  <!-- page-sizes 每页显示几条
-   current-page 当前显示页
-   total 一共多少条 res.data返回的total -->
+    <!-- 分页 -->
+    <!-- page-sizes 每页显示几条
+    current-page 当前显示页
+    total 一共多少条 res.data返回的total -->
    <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
@@ -54,6 +54,7 @@
     </el-pagination>
     <!-- 添加功能弹出的Dialog对话框放在最上面或者最下面 -->
     <el-dialog
+      @close = "closeDialog"
       title="添加用户"
       :visible.sync="dialogVisible"
       width="40%">
@@ -75,6 +76,24 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addUsers">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 编辑的谈话框 -->
+    <el-dialog title="修改用户" :visible.sync="editVisible" width="40%">
+      <el-form ref="editForm" :model="editForm" label-width="80px" :rules="rules" >
+        <el-form-item label="用户名">
+          <el-tag type="info">{{editForm.username}}</el-tag>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email" placeholder="请输入邮箱"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editForm.mobile" placeholder="请输入手机"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editVisible = false">取消</el-button>
+        <el-button @click="editUsers">确定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -112,6 +131,12 @@ export default {
         mobile: [
           { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号', trigger: ['blur', 'change'] }
         ]
+      },
+      editVisible: false,
+      editForm: {
+        id: '',
+        email: '',
+        mobile: ''
       }
     }
   },
@@ -142,6 +167,7 @@ export default {
           `users/${row.id}/state/${row.mg_state}`
         )
         .then(res => {
+          // 只是按钮显示状态变化即可，不需要重新渲染
           console.log(res.data)
         })
     },
@@ -166,56 +192,128 @@ export default {
       this.getData()
     },
     // 根据id进行删除
-    deleteUsers (id) {
-      this.$confirm('确定要删除此用户信息吗？', '温馨提示', {
-        type: 'warning'
-      }).then(() => {
-        // 发送axios请求
-        this.$axios.delete(`users/${id}`).then(res => {
-          console.log('成功')
-          const { meta } = res.data // 对象解构赋值
-          if (meta.status === 200) {
-            this.$message.success('删除成功')
-            // 如果当前页只有一条数据了，还进行删除操作，应当当前页pagenum-1
-            if (this.tableData.length === 1 && this.pagenum > 1) {
-              this.pagenum--
-            }
-            // tableData的长度就是当前页的条数 this.tableData = res.data.data.users
-            // 重新渲染 渲染之前做判断
-            this.getData()
+    // deleteUsers (id) {
+    //   this.$confirm('确定要删除此用户信息吗？', '温馨提示', {
+    //     type: 'warning'
+    //   }).then(() => {
+    //     // 发送axios请求
+    //     this.$axios.delete(`users/${id}`).then(res => {
+    //       console.log('成功')
+    //       const { meta } = res.data // 对象解构赋值
+    //       if (meta.status === 200) {
+    //         this.$message.success('删除成功')
+    //         // 如果当前页只有一条数据了，还进行删除操作，应当当前页pagenum-1
+    //         if (this.tableData.length === 1 && this.pagenum > 1) {
+    //           this.pagenum--
+    //         }
+    //         // tableData的长度就是当前页的条数 this.tableData = res.data.data.users
+    //         // 重新渲染 渲染之前做判断
+    //         this.getData()
+    //       }
+    //     })
+    //   })
+    // },
+    // 使用 async和await 去优化代码
+    async deleteUsers (id) {
+      try {
+        await this.$confirm('确定要删除此用户信息吗?', '温馨提示', { type: 'warning' })
+        const { data } = await this.$axios.delete(`users/${id}`)
+        // console.log(data)
+        if (data.meta.status === 200) {
+          this.$message.success('删除成功')
+          if (this.tableData.length === 1 && this.pagenum > 1) {
+            this.pagenum--
           }
-        })
-      })
+          this.getData()
+        } else {
+          this.$message.error(data.meta.msg)
+        }
+      } catch (error) {
+        // console.log(error)
+        this.$message('取消成功')
+      }
     },
-    // 添加功能 点击添加按通过 dialogVisible属性控制是否显示对话框
     add () {
       this.dialogVisible = true
     },
-    addUsers () {
-      // 发送axios请求
-      //
-      // 表单验证通过事件
-      this.$refs.form.validate((valid) => {
-        if (valid) {
-          // 发送axios
-          this.$axios.post('users', this.form
-          // {
-          //   username: this.form.username,
-          //   password: this.form.password,
-          //   email: this.form.email,
-          //   mobile: this.form.monile
-          // }
-          ).then(res => {
-            // console.log(res.data)
-            // 提示添加成功 关闭对话框 重新渲染
-            this.$message.success('添加成功')
-            this.dialogVisible = false
-            this.getData()
-          })
-        } else {
-          // console.log(2)
+    // addUsers () {
+    //   // 发送axios请求
+    //   //
+    //   // 表单验证通过事件
+    //   this.$refs.form.validate((valid) => {
+    //     if (valid) {
+    //       // 发送axios
+    //       this.$axios.post('users', this.form
+    //       // {
+    //       //   username: this.form.username,
+    //       //   password: this.form.password,
+    //       //   email: this.form.email,
+    //       //   mobile: this.form.monile
+    //       // }
+    //       ).then(res => {
+    //         // console.log(res.data)
+    //         // 提示添加成功 关闭对话框 重新渲染
+    //         this.$message.success('添加成功')
+    //         this.dialogVisible = false
+    //         this.getData()
+    //       })
+    //     } else {
+    //       // console.log(2)
+    //     }
+    //   })
+    // }
+    // 使用async 和 await 优化代码
+    async addUsers () {
+      // 表单校验有成功和失败 用try catch
+      try {
+        await this.$refs.form.validate()
+        // 往下执行说明验证成功了 发送ajax请求
+        const res = await this.$axios.post('users', this.form)
+        const { meta } = res.data
+        if (meta.status === 201) {
+          this.$message.success('添加成功')
+          this.dialogVisible = false
+          // 如果当前每页显示4条数据，已经有了4条数据，再添加一条的话此时应该显示的时下一页。所以将当前页更新成最后一页 总条数/每页对少条  先让total + 1
+          this.total++
+          this.pagenum = Math.ceil(this.total / this.pagesize)
+          // 再重新渲染
+          this.getData()
+          // 关闭谈话框的时候应该清空内容
         }
-      })
+      } catch (error) {
+        this.$message.error()
+      }
+    },
+    closeDialog (form) {
+      // 表单重置方法
+      this.$refs.form.resetFields()
+    },
+    // 点击显示编辑框
+    showEditDialog (row) {
+      // 按钮上可以传整个的数据row
+      this.editVisible = true
+      this.editForm.id = row.id
+      this.editForm.email = row.email
+      this.editForm.mobile = row.mobile
+      this.editForm.username = row.username
+    },
+    async editUsers () {
+      // 验证通过发送ajax请求
+      try {
+        await this.$refs.editForm.validate()
+        // 校验成功往下执行
+        const { id, email, mobile } = this.editForm
+        const res = await this.$axios.put(`users${id}`, { email, mobile })
+        const { meta } = res.data
+        if (meta.status === 200) {
+          this.$message.success(meta.msg)
+          this.editVisible = false
+          this.getData()
+        } else { this.$message.error(meta.msg) }
+        console.log(res)
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 }
